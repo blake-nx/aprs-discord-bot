60ad91602230c5307c049c15430bf170f163e127
Brandon Brown <brandon@brandonb.ca>
2023-08-28 00:38:06 -0700

    Refactor to filter out unavailable data in fields

--- src/modules/getLocationInfo.js
+++ src/modules/getLocationInfo.js
@@ -1,7 +1,7 @@
 import { EmbedBuilder } from 'discord.js';
 import got from '../utils/got.js';
 import config from '../utils/loadConfig.js';
-import { DATE_OPTIONS, UNAVAILABLE } from '../utils/enums.js';
+import { DATE_OPTIONS } from '../utils/enums.js';
 
 export async function getLocationInfo(callsign, message) {
   if (!callsign) {
@@ -20,35 +20,36 @@ export async function getLocationInfo(callsign, message) {
     } else {
       let lat = data.entries[0].lat;
       let lng = data.entries[0].lng;
-      let comment = data.entries[0].comment || UNAVAILABLE;
+      let comment = data.entries[0].comment || null;
       let altitude = data.entries[0].altitude
         ? `${data.entries[0].altitude}m (${Math.round(data.entries[0].altitude * 3.28084)}ft)`
-        : UNAVAILABLE;
+        : null;
       let coords = `${lat},${lng}`;
       let timeUpdated = new Date(data.entries[0].time * 1000);
       let lastUpdated = new Date(data.entries[0].lasttime * 1000);
       let miniMapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${coords}&zoom=13&size=600x300&maptype=roadmap&markers=color:red|${coords}&key=${config.gmaps_token}`;
+      const fields = [
+        { name: 'Coordinates', value: coords },
+        altitude && { name: 'Altitude', value: altitude },
+        comment && { name: 'Beacon comment', value: comment },
+        {
+          name: 'First Beacon at position',
+          value: timeUpdated.toLocaleString('en-US', DATE_OPTIONS),
+        },
+        {
+          name: 'Last Beacon at position',
+          value: lastUpdated.toLocaleString('en-US', DATE_OPTIONS),
+        },
+        {
+          name: 'GMaps',
+          value: `https://www.google.com/maps/search/?api=1&query=${coords}`,
+        },
+      ].filter(Boolean);
       message.channel.send({
         embeds: [
           new EmbedBuilder()
             .setColor(config.embed_color)
-            .addFields([
-              { name: 'Coordinates', value: coords },
-              { name: 'Altitude', value: altitude },
-              { name: 'Beacon comment', value: comment },
-              {
-                name: 'First Beacon at position',
-                value: timeUpdated.toLocaleString('en-US', DATE_OPTIONS),
-              },
-              {
-                name: 'Last Beacon at position',
-                value: lastUpdated.toLocaleString('en-US', DATE_OPTIONS),
-              },
-              {
-                name: 'GMaps',
-                value: `https://www.google.com/maps/search/?api=1&query=${coords}`,
-              },
-            ])
+            .addFields(fields)
             .setImage(miniMapUrl)
             .setTimestamp()
             .setFooter({ text: 'Source: https://aprs.fi' }),
--- src/modules/getWeather.js
+++ src/modules/getWeather.js
@@ -1,7 +1,7 @@
 import { EmbedBuilder } from 'discord.js';
 import got from '../utils/got.js';
 import config from '../utils/loadConfig.js';
-import { DATE_OPTIONS, UNAVAILABLE } from '../utils/enums.js';
+import { DATE_OPTIONS } from '../utils/enums.js';
 
 export async function getWeather(callsign, message) {
   if (!callsign) {
@@ -18,43 +18,44 @@ export async function getWeather(callsign, message) {
         "Sorry, I couldn't find that. Please check the callsign and try again."
       );
     } else {
-      let temp = data.entries[0].temp ? `${data.entries[0].temp}C` : UNAVAILABLE;
-      let pressure = data.entries[0].pressure || UNAVAILABLE;
-      let humidity = data.entries[0].humidity || UNAVAILABLE;
-      let wind_direction = data.entries[0].wind_direction || UNAVAILABLE;
-      let wind_speed = data.entries[0].wind_speed || UNAVAILABLE;
-      let wind_gust = data.entries[0].wind_gust || UNAVAILABLE;
-      let rain_1h = data.entries[0].rain_1h ? `${data.entries[0].rain_1h}mm` : UNAVAILABLE;
-      let rain_24h = data.entries[0].rain_24h ? `${data.entries[0].rain_24h}mm` : UNAVAILABLE;
-      let rain_mn = data.entries[0].rain_mn ? `${data.entries[0].rain_mn}mm` : UNAVAILABLE;
-      let luminosity = data.entries[0].luminosity || UNAVAILABLE;
+      let temp = data.entries[0].temp ? `${data.entries[0].temp}C` : null;
+      let pressure = data.entries[0].pressure || null;
+      let humidity = data.entries[0].humidity || null;
+      let wind_direction = data.entries[0].wind_direction || null;
+      let wind_speed = data.entries[0].wind_speed || null;
+      let wind_gust = data.entries[0].wind_gust || null;
+      let rain_1h = data.entries[0].rain_1h ? `${data.entries[0].rain_1h}mm` : null;
+      let rain_24h = data.entries[0].rain_24h ? `${data.entries[0].rain_24h}mm` : null;
+      let rain_mn = data.entries[0].rain_mn ? `${data.entries[0].rain_mn}mm` : null;
+      let luminosity = data.entries[0].luminosity || null;
       let timeUpdated = new Date(data.entries[0].time * 1000);
       let lastUpdated = new Date(data.entries[0].lasttime * 1000);
       let miniMapUrl = `http://www.findu.com/cgi-bin/radar-find.cgi?call=${callsign}`;
+      const fields = [
+        temp && { name: 'Temp', value: temp },
+        pressure && { name: 'Pressure', value: pressure },
+        humidity && { name: 'Humidity', value: humidity },
+        wind_direction && { name: 'Wind direction', value: wind_direction },
+        wind_speed && { name: 'Wind speed', value: wind_speed },
+        wind_gust && { name: 'Wind gust', value: wind_gust },
+        rain_1h && { name: 'Rainfall past 1hr', value: rain_1h },
+        rain_24h && { name: 'Rainfall past 24hrs', value: rain_24h },
+        rain_mn && { name: 'Rainfall since midnight', value: rain_mn },
+        luminosity && { name: 'Luminosity', value: luminosity },
+        {
+          name: 'First Beacon at position',
+          value: timeUpdated.toLocaleString('en-US', DATE_OPTIONS),
+        },
+        {
+          name: 'Last Beacon at position',
+          value: lastUpdated.toLocaleString('en-US', DATE_OPTIONS),
+        },
+      ].filter(Boolean);
       message.channel.send({
         embeds: [
           new EmbedBuilder()
             .setColor(config.embed_color)
-            .addFields([
-              { name: 'Temp', value: temp },
-              { name: 'Pressure', value: pressure },
-              { name: 'Humidity', value: humidity },
-              { name: 'Wind direction', value: wind_direction },
-              { name: 'Wind speed', value: wind_speed },
-              { name: 'Wind gust', value: wind_gust },
-              { name: 'Rainfall past 1hr', value: rain_1h },
-              { name: 'Rainfall past 24hrs', value: rain_24h },
-              { name: 'Rainfall since midnight', value: rain_mn },
-              { name: 'Luminosity', value: luminosity },
-              {
-                name: 'First Beacon at position',
-                value: timeUpdated.toLocaleString('en-US', DATE_OPTIONS),
-              },
-              {
-                name: 'Last Beacon at position',
-                value: lastUpdated.toLocaleString('en-US', DATE_OPTIONS),
-              },
-            ])
+            .addFields(fields)
             .setImage(miniMapUrl)
             .setTimestamp()
             .setFooter({ text: 'Source: https://aprs.fi' }),
