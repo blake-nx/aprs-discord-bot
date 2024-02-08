1f3f05b427c80c0e939f4e8252a224721d9be7a3
Brandon Brown <brandon@brandonb.ca>
2024-02-08 09:31:39 -0800

    Proper fallbacks, not using double-pipe

--- src/utils/loadConfig.js
+++ src/utils/loadConfig.js
@@ -1,9 +1,12 @@
 import fsExtra from 'fs-extra';
 
-function getEnv(name) {
+function getEnv(name, fallback = null) {
   let val = process.env[name];
   if (val === undefined || val === null) {
-    throw `Missing environment variale for ${name}`;
+    if (fallback !== null) {
+      throw `Missing environment variale for ${name}`;
+    }
+    return fallback;
   }
   return val;
 }
@@ -18,14 +21,15 @@ if (fsExtra.existsSync(jsonFile)) {
   // Allow environment variables to be defined as well for coinfig values
   config = {
     token: getEnv('BOT_DISCORD_TOKEN'),
-    prefix: getEnv('BOT_MSG_PREFIX') || '?',
+    prefix: getEnv('BOT_MSG_PREFIX', '?'),
     aprs_token: getEnv('BOT_APRSFI_TOKEN'),
     gmaps_token: getEnv('BOT_GMAPS_TOKEN'),
-    timezone: getEnv('BOT_TIMEZONE') || 'America/Vancouver',
-    embed_color: getEnv('BOT_EMBED_COLOR') || '#efefef',
-    user_agent:
-      getEnv('BOT_USER_AGENT') ||
-      'aprs-discord-bot v1 (+https://github.com/brandonb927/aprs-discord-bot)',
+    timezone: getEnv('BOT_TIMEZONE', 'America/Vancouver'),
+    embed_color: getEnv('BOT_EMBED_COLOR', '#efefef'),
+    user_agent: getEnv(
+      'BOT_USER_AGENT',
+      'aprs-discord-bot v1 (+https://github.com/brandonb927/aprs-discord-bot)'
+    ),
   };
 }
 console.log(config);
